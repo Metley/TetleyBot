4bdbd0b69376f6e2d1045508c2dcd41e23028f4e
Metley <37466817+Metley@users.noreply.github.com>
2018-03-17 03:14:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,20 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const schedule = require('node-schedule');
-var count = 0;
+var count = 1;
 
 
 client.on("message", async message => {
 
-    if (message.content == '!StartClock') {
+    if (message.content == '!startclock') {
         message.channel.sendMessage('Clock has started');
-        var j = schedule.scheduleJob('01 * * * *', function () {
+        var j = schedule.scheduleJob('15 * * * *', function () {
             message.guild.channels.find("name", "general").send("There has been " + count + " days without a problem");
             count++;
         });
     }
 
-    if (message.content == '!ResetClock') {
+    if (message.content == '!resetclock') {
         message.channel.sendMessage('Clock has been Reset. 😢');
         count = 0;
     }
