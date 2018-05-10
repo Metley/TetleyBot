0af4b982e24567dfa48c61474b6d608986592256
Metley <37466817+Metley@users.noreply.github.com>
2018-05-10 01:04:17 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const schedule = require('node-schedule');
 const gyms = require ("./gym.json");
-var count = 1;
 
 
 client.on("message", async message => {
@@ -16,8 +15,8 @@ client.on("message", async message => {
 
    		
     if(message.content == '!test'){
-	message.guild.channels.find("name", "general").send('hello');
-     }
+		message.guild.channels.find("name", "general").send('hello');
+    }
 	
 	
 	if(command == '!find'){
@@ -30,11 +29,27 @@ client.on("message", async message => {
 									      +"\n**EX Eligible:** " +gymdb[gym].exeligible
 									      +"\n**Nearby Intersection:** "+gymdb[gym].nearbyintersection
 									      +"\n**Notes:** "+gymdb[gym].notes);
-			break;
-			   }
+				break;
+			}
 		}
 
  	}
+	
+	if(command === "purge") {
+		// This command removes all messages from all users in the channel, up to 100.
+		
+		// get the delete count, as an actual number.
+		const deleteCount = parseInt(args[0], 10);
+		
+		// Ooooh nice, combined conditions. <3
+		if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+		  return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+		
+		// So we get our messages, and delete them. Simple enough, right?
+		const fetched = await message.channel.fetchMessages({count: deleteCount});
+		message.channel.bulkDelete(fetched)
+		  .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+	}
 
 });
 
