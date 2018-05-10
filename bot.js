bc07944999017b59b1044fca4b51c9d04f7d0b30
Metley <37466817+Metley@users.noreply.github.com>
2018-05-10 00:57:38 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,21 +14,7 @@ client.on("message", async message => {
     const command = args.shift();
         
 
-    if (message.content == '!startgymclock') {
-        message.channel.sendMessage('Clock has started');
-        var j = schedule.scheduleJob('15 * * * *', function () {
-            message.guild.channels.find("name", "general").send("There has been " + count + " days without a channel wipe!");
-            count++;
-        });
-    }
-
-    if (message.content == '!resetgymclock') {
-        //message.channel.sendMessage
-        message.guild.channels.find("name", "general").send('Wipe has occured time to reset. ðŸ˜¢');
-        count = 1;
-    }
-	
-	
+   		
     if(message.content == '!test'){
 	message.guild.channels.find("name", "general").send('hello');
      }
