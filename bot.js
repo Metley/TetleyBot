326d31c774061446db9400892a65400e54806524
Metley <37466817+Metley@users.noreply.github.com>
2018-06-13 13:31:02 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -131,7 +131,7 @@ client.on("message", async message => {
 			for(var gym in gymdb){
 				if(gymdb[gym].nickname.toLowerCase().includes(locations[i])){
 					var qrole = message.channel.guild.roles.get('name', gymdb[gym].quadrant);
-				   output += qrole.mention() +" \n"+"**" +gymdb[gym].gymname +": **" 
+				   output += qrole.toString() +" \n"+"**" +gymdb[gym].gymname +": **" 
 					   +gymdb[gym].gymlocation+"\n**Notes:** "+gymdb[gym].notes+"\n\n";
 					found = 1;
 					ncheck = false;
@@ -142,7 +142,7 @@ client.on("message", async message => {
 				for(var gym in gymdb){
 					if(gymdb[gym].gymname.toLowerCase().includes(locations[i])){
 						var qrole = message.channel.guild.roles.get('name', gymdb[gym].quadrant);
-					   output += qrole.mention() +" \n"+"**" +gymdb[gym].gymname +": **" 
+					   output += qrole.toString() +" \n"+"**" +gymdb[gym].gymname +": **" 
 						   +gymdb[gym].gymlocation+"\n**Notes:** "+gymdb[gym].notes+"\n\n";
 						found = 1;
 						break;
