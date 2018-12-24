bb97a92334bde453f6d4ef20faa0925e6b465743
Metley <37466817+Metley@users.noreply.github.com>
2018-12-23 21:25:04 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -214,7 +214,13 @@ client.on("message", async message => {
 		message.channel.send('whatup '+person+ ' '+id);
 		
 	}
-	
+	if((command == '!printSecret')&&(message.author.id == '327162272990363648')){
+		var badgedb = badgesdb.badgedb;
+		
+		for(var badge in badgedb){
+			message.channel.send(badgedb[badge].id +"\n" +badgedb[badge].badge1+"\n" +badgedb[badge].badge2+"\n" +badgedb[badge].badge3);
+		}
+	}
 	
 	
 	
