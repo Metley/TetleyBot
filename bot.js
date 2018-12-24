3f1425750d3689c22bc73944f33a6d9d7ba10332
Metley <37466817+Metley@users.noreply.github.com>
2018-12-23 19:51:55 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ client.on("message", async message => {
 
    		
     if((message.content == '!test')&&(message.author.id == '327162272990363648')){
-		message.channel.send('hello <:Pokeball:526561021750673418>');
+		message.channel.send('hello <a:Pokeball:526561021750673418>');
     }
 	
 	if((message.content.toLowerCase().includes('more eggs'))&&(message.channel.id == '426757841580195850')){
